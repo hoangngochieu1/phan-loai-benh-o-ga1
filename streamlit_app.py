ed403e6e9bfbabf29d4c9c8ffc72229c2b5a4c5f
hoangngochieu1 <hieuhn.22ds@vku.udn.vn>
2024-12-25 17:36:19 +0000

    ad

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
+st.titádle("🎈 My new app")
+st.wriádete(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
